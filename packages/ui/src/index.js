b8a06a48f631b1becdf555ce3f39ec9e6e6edb26
Bakate <bakateba@gmail.com>
2026-05-02 08:26:57 +0200

    refactor(ui): remove barrel file, import components directly by path

--- packages/ui/src/index.js
+++
@@ -1,4 +0,0 @@
-export { createButton, createSpinner } from './components/Button.js';
-export { createInput, showFieldError, clearFieldError } from './components/Input.js';
-export { createForm, getFormValues, setFormError, clearFormError } from './components/Form.js';
-export { createModal } from './components/Modal.js';
